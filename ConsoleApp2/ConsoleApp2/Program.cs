d8a3b935ee6246850a61190bc681b9d176a1d37c
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2020-09-01 12:30:51 +0200

    läxa 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down your name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name);
+
+
+            Console.WriteLine("Write down your age");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("" + age);
+
+            Console.WriteLine("Are you alive?");
+
+            string answear = Console.ReadLine();
+
+            switch (answear)
+            {
+                case "yes":
+                    Console.WriteLine("True");
+                    break;
+                case "no":
+                    Console.WriteLine("false");
+                    break;
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
