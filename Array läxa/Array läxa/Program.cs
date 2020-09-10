3779ab1b8bea7dc873a9a815d69a8358b17b7acd
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2020-09-10 11:06:45 +0200

    arrayLäxa

---
+++ Array läxa/Array läxa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_läxa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new string[5];
+
+            Console.WriteLine("Write 5 words that u wanted reverted in a list");
+
+            for (int i = 0; i < 5; i++)
+            {
+                words[i] = Console.ReadLine();
+
+            }
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+
+            }
+
+        }
+    }
+}
